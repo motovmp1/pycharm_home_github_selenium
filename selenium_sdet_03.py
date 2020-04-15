79481b5dc3f86c25ec058db89e9b0a5af5f3f7ca
motovmp1 <vinicius.mpinho@gmail.com>
2020-04-15 18:03:44 +0800

    running test at home

---
+++ selenium_sdet_03.py
@@ -0,0 +1,62 @@
+# This is a github selenium with pycharm - Test all elements for second time
+import time
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+
+driver.get("http://www.newtours.demoaut.com/")
+# This is a title of the page
+time.sleep(1)
+driver.maximize_window()
+time.sleep(1)
+print(driver.title)
+
+# This is get URL of the page
+time.sleep(1)
+print(driver.current_url)
+
+driver.get("http://www.pavantestingtools.com/")
+
+# This is a title of the page
+time.sleep(1)
+print(driver.title)
+
+# This is get URL of the page
+time.sleep(1)
+print(driver.current_url)
+
+# This is go back
+time.sleep(4)
+driver.back()
+print("I ma inside the <-- back button")
+
+# This is a title of the page
+time.sleep(1)
+print(driver.title)
+
+# This is get URL of the page
+time.sleep(1)
+print(driver.current_url)
+
+# This is return HTML code form the page
+# print(driver.page_source)
+time.sleep(3)
+driver.forward()
+print("I ma inside the forward --> button")
+
+# This is a title of the page
+time.sleep(1)
+print(driver.title)
+
+# This is get URL of the page
+time.sleep(1)
+print(driver.current_url)
+
+time.sleep(5)
+
+# This is close only one window
+driver.close()
+
+# quit command can close all the browsers at the same time
