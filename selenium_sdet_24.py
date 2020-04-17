3aa70bd0d5b7adadcd58a9e600b2cd0885b56a2c
motovmp1 <vinicius.mpinho@gmail.com>
2020-04-17 22:15:59 +0800

    running test at home 5

---
+++ selenium_sdet_24.py
@@ -0,0 +1,17 @@
+import openpyxl
+import time
+
+path = "/home/elsys/Desktop/Teste/pycharm_home_github_selenium/readexcel.xlsx"
+
+workbook = openpyxl.load_workbook(path)
+
+# sheet = workbook.get_sheet_by_name("Sheet1")
+
+sheet = workbook.active
+
+rows = sheet.max_row
+cols = sheet.max_column
+
+print(rows)
+print(cols)
+
