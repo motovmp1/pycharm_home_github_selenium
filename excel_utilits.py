5ab6f5120d649fa06eb79dae621f3780ea16981d
motovmp1 <vinicius.mpinho@gmail.com>
2020-04-18 23:57:25 +0800

    running test at home 12

--- excel_utilits.py
+++ excel_utilits.py
@@ -1,5 +1,13 @@
 # from class 24 and 25 for excel file use
 import openpyxl
+from openpyxl.styles import PatternFill
+from openpyxl import load_workbook
+from openpyxl.styles import colors
+from openpyxl.styles import Font, Color
+
+redFill = PatternFill(start_color='FFFF0000',
+                      end_color='FFFF0000',
+                      fill_type='solid')
 
 
 def get_row_count(file, sheet_name):
@@ -24,4 +32,24 @@ def write_data_file(file, sheet_name, row_num, column_num, data):
     workbook = openpyxl.load_workbook(file)
     sheet = workbook.get_sheet_by_name(sheet_name)
     sheet.cell(row=row_num, column=column_num).value = data
+    sheet['A1'].fill = PatternFill(start_color="FFC7CE", end_color="FFC7CE", fill_type="solid")
+    # sheet['A1'].fill = redFill
+    workbook.save(file)
+
+
+def change_cell_color_fail(file, sheet_name, row_num, column_num, data, cell):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook.get_sheet_by_name(sheet_name)
+    sheet.cell(row=row_num, column=column_num).value = data
+    sheet[cell].fill = PatternFill(start_color="FFC7CE", end_color="FFC7CE", fill_type="solid")
+    # sheet['A1'].fill = redFill
+    workbook.save(file)
+
+
+def change_cell_color_pass(file, sheet_name, row_num, column_num, data, cell):
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook.get_sheet_by_name(sheet_name)
+    sheet.cell(row=row_num, column=column_num).value = data
+    sheet[cell].fill = PatternFill(start_color="CCFFE5", end_color="CCFFE5", fill_type="solid")
+    # sheet['A1'].fill = redFill
     workbook.save(file)
--- selenium_sdet_26.py
+++ selenium_sdet_26.py
@@ -4,6 +4,11 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import excel_utilits
+import openpyxl
+from openpyxl.styles import Font, Color, Alignment, Border, Side, colors
+from openpyxl.styles import PatternFill, colors
+
+red_background = PatternFill(bgColor=colors.RED)
 
 driver = webdriver.Firefox()
 
@@ -29,15 +34,31 @@ numbers_rows = excel_utilits.get_row_count(path, "Sheet1")
 print(numbers_rows)
 
 for r in range(2, numbers_rows + 1):
+    print(type(r))
+    cell_color = ('C' + str(r))
+    print(cell_color)
+    print(cell_color)
     username = excel_utilits.read_data_file(path, "Sheet1", r, 1)
     password = excel_utilits.read_data_file(path, "Sheet1", r, 2)
 
-    driver.find_element_by_xpath("//input[@name='userName']").send_keys("")
+    driver.find_element_by_xpath("//input[@name='userName']").send_keys(username)
     time.sleep(2)
-    driver.find_element_by_xpath("//input[@name='password']").send_keys("")
+    driver.find_element_by_xpath("//input[@name='password']").send_keys(password)
     time.sleep(2)
     driver.find_element_by_xpath("//input[@name='login']").click()
+    time.sleep(25)
 
+    if driver.title == "Find a Flight: Mercury Tours:":
+        print(driver.title)
+        print("Test is PASS")
+        excel_utilits.write_data_file(path, "Sheet1", r, 3, "PASS")
+        excel_utilits.change_cell_color_pass(path, "Sheet1", r, 3, "PASS", cell_color)
+    else:
+        print("Test is FAIL")
+        excel_utilits.write_data_file(path, "Sheet1", r, 3, "FAIL")
+        excel_utilits.change_cell_color_fail(path, "Sheet1", r, 3, "FAIL", cell_color)
+    driver.find_element_by_link_text("Home").click()
+    time.sleep(4)
 # ***************************** End test case   *******************************************
 
 
