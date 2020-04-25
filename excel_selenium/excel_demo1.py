f6d15e5c689baf97ec7fd33eeb35051d4f0b151a
motovmp1 <vinicius.mpinho@gmail.com>
2020-04-25 11:15:38 +0800

    running test at home 33

--- excel_selenium/excel_demo1.py
+++ excel_selenium/excel_demo1.py
@@ -24,3 +24,30 @@ print(sheet['A5'])
 # value inside the cell
 print(sheet['A5'].value)
 
+print("###########################################################")
+# this is max number
+for interaction in range(1, sheet.max_row+1):
+    print(sheet.cell(row=interaction, column=1).value, end=" | ")
+print("")
+print("############################################################")
+
+
+
+
+print("###########################################################")
+# this is max number
+for interaction in range(1, sheet.max_row+1):
+    # quando o teste bate imprimi apenas a linha que interessa
+    if sheet.cell(row=interaction, column=1).value == "Testcase2":
+        for join in range(1, sheet.max_column+1):
+            print(sheet.cell(row=interaction, column=join).value)
+print("")
+print("############################################################")
+
+
+cell = sheet['A1': 'D7']
+
+for c1, c2, c3, c4 in cell:
+    print("{0:8}    {1:8}   {2:8}    {3:8}".format(c1.value, c2.value, c3.value, c4.value))
+
+print("############################################################")
